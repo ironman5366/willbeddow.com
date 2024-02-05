26cbca926251199a82b36ac4ba9c80e2a88055e0
Will Beddow <will@willbeddow.com>
2024-02-05 18:18:27 -0500

    more WIP on everything

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,24 @@
-import SquigglyCard from "@/components/molecules/SquigglyCard";
-import {Center} from "@mantine/core";
+import {Card, Center, Text, Title} from "@mantine/core";
+import flower from "../../public/flower.svg"
+
 export default function Home() {
-  return <Center>
-      <SquigglyCard>
-          Hello world
-      </SquigglyCard>
-  </Center>
+  return <div style={{
+      backgroundImage: `url(${flower.src})`,
+      backgroundRepeat: "repeat",
+      minHeight: "100vh",
+      minWidth: "100vw",
+      backgroundColor: "#fefefa"
+  }}>
+      <Center>
+          <Card shadow={"xl"} style={{
+          }}>
+              <Title>
+                  Hi, I&apos;m Will.
+              </Title>
+              <Text>
+                  I like writing code, being outside, and websites that feel cozy.
+              </Text>
+          </Card>
+      </Center>
+  </div>
 }
--- app/src/components/molecules/SquigglyCard.tsx
+++ app/src/components/molecules/SquigglyCard.tsx
@@ -1,10 +1,16 @@
 import React, { PropsWithChildren } from "react";
 import "./squiggle.css"
+import SquiggleBox from '../../../public/squiggle/box.svg'
 
 export default function SquigglyCard({ children }: PropsWithChildren<any>) {
 
     return (
-        <div className="squiggle-bottom squiggle-top">
+        <div style={{
+            border: '10px solid transparent', // Set the border width
+            borderImage: `url(${SquiggleBox.src}) 30 30 30 30 fill`,
+            borderImageSlice: '30 fill', // Adjust slice values
+            borderImageRepeat: 'stretch', // Repeat the image to avoid stretching
+        }}>
             {children}
         </div>
     );
