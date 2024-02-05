a3aafb4c8581c90a8ff1f9e0428efb718bb4abe4
Will Beddow <will@willbeddow.com>
2024-02-05 18:35:48 -0500

    Flex center

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,7 @@
-import {Card, Center, Text, Title} from "@mantine/core";
+'use client'
+import {Card, Center, Container, Flex, Grid, Text, Title} from "@mantine/core";
 import flower from "../../public/flower.svg"
+import HeroCard from "../components/atoms/HeroCard";
 
 export default function Home() {
   return <div style={{
@@ -10,15 +12,17 @@ export default function Home() {
       backgroundColor: "#fefefa"
   }}>
       <Center>
-          <Card shadow={"xl"} style={{
-          }}>
-              <Title>
-                  Hi, I&apos;m Will.
-              </Title>
-              <Text>
-                  I like writing code, being outside, and websites that feel cozy.
-              </Text>
-          </Card>
+          <Container>
+              <Flex
+                  align="center"
+                  justify="center"
+                  style={{ height: '100vh' }} // This makes the Flex container fill the viewport height
+              >
+                  <Card shadow="sm" p="lg" style={{ width: '300px', textAlign: 'center' }}>
+                      <Text>This is a centered card.</Text>
+                  </Card>
+              </Flex>
+          </Container>
       </Center>
   </div>
 }
---
+++ app/src/components/atoms/HeroCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Card, Text, Title} from "@mantine/core";
+
+export default function HeroCard() {
+    return <Card shadow={"xl"} style={{
+    }}>
+        <Title>
+            Hi, I&apos;m Will.
+        </Title>
+        <Text>
+            I like writing code, being outside, and websites that feel cozy.
+        </Text>
+    </Card>
+}
